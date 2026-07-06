401adf92aa6e2c9f7f2dc6d1477cf31be86434f7
Mayank <146189721+mayank9142@users.noreply.github.com>
2026-07-06 21:57:27 +0530

    Binary Search

---
+++ _6Binary_Search/_8_arranging_coins.java
@@ -0,0 +1,37 @@
+package _6Binary_Search;
+
+public class _8_arranging_coins {
+    class Solution {
+        // public int mySqrt(long n) {
+        //     if(n==0) return 0;
+        //     long lo=1;
+        //     long hi=n;
+
+        //     while(lo<=hi){
+        //         long mid= lo + (hi-lo)/2;
+        //         if(mid==n/mid) return (int)mid;
+        //         else if(mid>n/mid) hi =mid-1;
+        //         else lo=mid+1;
+        //     }
+        //     return (int)hi;
+        // }
+        public int arrangeCoins(int n) {
+//         long m =n;
+//         return  (int)(Math.sqrt(8*m+1)-1)/2;
+            long lo=0,hi=n,ans=0;
+            while(lo<=hi){
+                long k = lo+(hi-lo)/2;
+                long m =k*(k+1)/2;
+                if(m==n) return (int)k;
+                else if (m>n){
+                    hi=k-1;
+                }
+                else{
+                    ans=k;
+                    lo=k+1;
+                }
+            }
+            return (int)(ans);
+        }
+    }
+}
