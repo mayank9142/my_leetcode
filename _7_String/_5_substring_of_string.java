8a1a86da011d3b4656369330ad5840acc70b999a
Mayank <146189721+mayank9142@users.noreply.github.com>
2026-07-08 13:23:04 +0530

    Strings

---
+++ _7_String/_5_substring_of_string.java
@@ -0,0 +1,9 @@
+package _7_String;
+
+public class _5_substring_of_string {
+    static void main(String[] args) {
+        String s ="jaishankar";
+        System.out.println(s.substring(3));//prints from index 3 to end
+        System.out.println(s.substring(3,8));//-->prints 3 to 8
+    }
+}
