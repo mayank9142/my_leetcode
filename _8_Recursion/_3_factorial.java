0bf78e5b98df5713d5c06fa6ffd910f3cc55197f
Mayank <146189721+mayank9142@users.noreply.github.com>
2026-07-11 19:20:32 +0530

    Recursion

---
+++ _8_Recursion/_3_factorial.java
@@ -0,0 +1,19 @@
+package _8_Recursion;
+
+import java.util.Scanner;
+
+public class _3_factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = factorial(n);
+        System.out.println(ans);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
