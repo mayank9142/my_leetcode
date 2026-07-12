efc5b200d9a1f8f47efcc656ae0e45261eb5f946
Mayank <146189721+mayank9142@users.noreply.github.com>
2026-07-12 23:54:57 +0530

    Recursion

---
+++ _8_Recursion/_8_GCD.java
@@ -0,0 +1,12 @@
+package _8_Recursion;
+
+public class _8_GCD {
+    class Solution {
+        public static int gcd(int a, int b) {
+            // code here
+            if(a==0) return b;
+            return gcd(b%a,a);
+        }
+    }
+
+}
